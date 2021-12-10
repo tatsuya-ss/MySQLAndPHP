a9e6427f65e210e615b7a107e6cfe8f2c4512b2f
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-10 16:07:58 +0900

    jsonを配列に変換

--- todo.php
+++ todo.php
@@ -4,6 +4,8 @@ require_once 'connectPDO.php';
 $sql = "SELECT * FROM todo";
 $results = $dbh->query($sql);
 
+echo "[";
 foreach ($results as $result) {
-    echo $result["jdoc"];
-}
+    echo $result["jdoc"].",";
+};
+echo "]";
